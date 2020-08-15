6699f41c8cb574895cf55aec2f36b6638fac127c
Yefer <yefercuber20@gmail.com>
2020-08-15 15:00:39 -0600

    se agrega el analizador

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yefer
+ */
+public class Main {
+    
+    public static void main( String[] args ){
+        Pantalla p = new Pantalla();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+        /* X=2,Y=2,Z=3; 2*X+Y*Z */
+    }
+}
